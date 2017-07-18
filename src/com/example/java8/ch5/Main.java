3bc1a73bb5dac44d7cd79996ffdf843726574ec0
john.k <john.k@daumkakao.com>
2017-07-18 10:59:09 +0900

    ou could use two maps to iterate on the two lists and generate the pairs

--- src/com/example/java8/ch5/Main.java
+++ src/com/example/java8/ch5/Main.java
@@ -70,6 +70,18 @@ public class Main {
 
         List<String> uniqueCharacters = words.stream().map(word -> word.split(""))
                 .flatMap(Arrays::stream).distinct().collect(Collectors.toList());
+
+        List<Integer> numbers = Arrays.asList(1, 2, 3, 4, 5);
+        List<Integer> squares = numbers.stream().map(n -> n*n).collect(Collectors.toList());
+
+        // [1, 2, 3] and a list [3, 4] you should return [(1, 3), (1, 4), (2, 3), (2, 4), (3, 3), (3, 4)].
+        List<Integer> numbers1 = Arrays.asList(1, 2, 3);
+        List<Integer> numbers2 = Arrays.asList(3, 4);
+        List<int[]> pairs = numbers1
+                .stream()
+                .flatMap(i -> numbers2.stream().map(j -> new int[]{i, j}))
+                .collect(Collectors.toList());
+        
     }
 
     
