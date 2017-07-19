5bb50766137f27a612d8d0e412d73e359fb101b0
john.k <john.k@daumkakao.com>
2017-07-19 16:01:58 +0900

    ambiguous method call,

--- src/com/example/java8/ch8/Main.java
+++ src/com/example/java8/ch8/Main.java
@@ -2,6 +2,10 @@ package com.example.java8.ch8;
 
 public class Main {
 
+    interface Task{
+        public void execute();
+    }
+
     public static void main(String[] args) {
 	    // write your code here
         // From anonymous classes to lambda expressions
@@ -13,6 +17,27 @@ public class Main {
         };
 
         Runnable r2 = () -> System.out.println("Hello");
+
+        doSomething(new Task() {
+            public void execute() {
+                System.out.println("Danger danger!!"); }
+        });
+
+        // But converting this anonymous class to a lambda expression results in an ambiguous method call, because both Runnable and Task are valid target types:
+
+        // doSomething( () -> System.out.println("Danger danger!!"));
+
+        // You can solve the ambiguity by providing an explicit cast (Task)
+        doSomething( (Task)() -> System.out.println("Danger danger!!"));
         
+        
+    }
+
+    public static void doSomething(Runnable r) {
+        r.run();
+    }
+
+    public static void doSomething(Task a) {
+        a.execute();
     }
 }
