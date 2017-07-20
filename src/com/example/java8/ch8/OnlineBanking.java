f3e480c9b53e8a7512828f9b379e6d7172c83e25
john.k <john.k@daumkakao.com>
2017-07-20 13:54:42 +0900

    Template method

---
+++ src/com/example/java8/ch8/OnlineBanking.java
@@ -0,0 +1,22 @@
+package com.example.java8.ch8;
+
+/**
+ * Created by shuaihan on 2017. 7. 20..
+ */
+public abstract class OnlineBanking {
+    /*
+    public void processCustomer(int id) {
+        Customer c = Database.getCustomerWithId(id);
+        makeCustomerHappy(c);
+    }
+
+
+    public void processCustomer(int id, Consumer<Customer> makeCustomerHappy) {
+        Customer c = Database.getCustomerWithId(id);
+        makeCustomerHappy.accept(c);
+    }
+
+
+    abstract void makeCustomerHappy(Customer c);
+    */
+}
